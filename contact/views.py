da701eb7f0e8e3392fe89395a36ed87d7d464e6b
nahidallhv <161585836+nahidallhv@users.noreply.github.com>
2024-10-18 17:00:16 +0400

    back

---
+++ contact/views.py
@@ -0,0 +1,25 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.core.mail import send_mail
+from rest_framework import status
+
+class ContactView(APIView):
+    def post(self, request):
+        name = request.data.get('name')
+        email = request.data.get('email')
+        message = request.data.get('message')
+
+        if not all([name, email, message]):
+            return Response({'success': False, 'error': 'All fields are required'}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            send_mail(
+                f"New Message from {name}",
+                message,
+                email,
+                ['senin-email@gmail.com'],  # Email alıcısı
+                fail_silently=False,
+            )
+            return Response({'success': True, 'message': 'Email sent successfully'}, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({'success': False, 'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
